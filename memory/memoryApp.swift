2e47fe944b1ca2ac056f549bbe6dbb3ecd3b0b34
Mazefest <colbymehmen@gmail.com>
2021-07-06 14:35:45 -0500

    Initial Commit

---
+++ memory/memoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoryApp.swift
+//  memory
+//
+//  Created by Colby Mehmen on 7/6/21.
+//
+
+import SwiftUI
+
+@main
+struct memoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
